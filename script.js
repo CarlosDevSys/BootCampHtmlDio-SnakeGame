94aad5230ae30da930cf4910f1fc15dbf70954e8
CarlosDevSys <carlos.sk8@hotmail.com>
2021-10-30 00:22:07 -0300

    Criadas funcoes

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box =32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+let direction = "right";
+
 
 function criarBG(){
   context.fillStyle = "Lightgreen";
@@ -8,4 +15,34 @@ function criarBG(){
 
 }
 
-criarBG();
+function criarSnake(){
+    for(i=0;i < snake.length;i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    }
+
+}
+
+function iniciarJogo{
+    criarBG();
+    criarSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeX += box;
+
+    snake.pop();
+
+    let newHead = {
+        y: snakeX,
+        x: snakeY
+    }
+
+    
+}
+
+let jogo = setInterval(iniciarJogo, 100);
