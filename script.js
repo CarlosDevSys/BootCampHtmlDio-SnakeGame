e743bb653c1ac12a0dbb36a28d6a3f5442190d65
CarlosDevSys <carlos.sk8@hotmail.com>
2021-11-05 00:28:04 -0300

    Implementado a pontuação dinamica e velocidade dinamica que aumenta conforme pontuação aumenta

--- script.js
+++ script.js
@@ -53,8 +53,6 @@ function iniciarJogo(){
         if(snake[0].x === snake[i].x && snake[0].y === snake[i].y){
             clearInterval(jogo);
             alert('Game Over :(');
-           iniciarJogo();
-
         }
     }
 
